ecaa9bec9cb9cec4d7912f8af8382e1b4c791d4f
Alex <alexmajko.1@gmail.com>
2021-01-08 18:48:52 +0300

    [*] add custom cell

---
+++ MyPump/MVC/Cells/MarkTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MarkTableViewCell.swift
+//  MyPump
+//
+//  Created by Александр Майко on 08.01.2021.
+//
+
+import UIKit
+
+class MarkTableViewCell: UITableViewCell {
+    @IBOutlet weak var markNameLabel: UILabel!
+    @IBOutlet weak var markImage: UIImageView!
+    
+
+}
