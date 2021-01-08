0303b8c14786f338be0c16c76879562026de6f7a
Alex <alexmajko.1@gmail.com>
2021-01-08 18:56:59 +0300

    [*] rename Firebase keys and child in code

--- MyPump/MVC/Managers/NetworkManager.swift
+++ MyPump/MVC/Managers/NetworkManager.swift
@@ -13,11 +13,12 @@ class NetworkManager {
     
     static func FetchModels(completion : @escaping(_ photoOfUnit:Model)->(Void)) {
         let ref = Database.database().reference()
-        ref.child("Model").observeSingleEvent(of: .value) { (snapshot) in
+        ref.child("Mark").observeSingleEvent(of: .value) { (snapshot) in
             guard let data = snapshot.value  else {return}
             if let data = data as? [String:Any] {
-                if let models = Mapper<Model>().map(JSONObject: data) {
-                    completion(models)
+                if let marksJSON = Mapper<Model>().map(JSONObject: data) {
+                    print(marksJSON.headerTitle ?? "")
+                    completion(marksJSON)
 //                    case .success(let value):
 //                        var courses = [Course]()
 //                        courses = Course.getArray(from: value)!
