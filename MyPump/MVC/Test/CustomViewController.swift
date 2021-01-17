121291c4f0cf5e77ee8d13ac391a093d351447b9
Alex <alexmajko.1@gmail.com>
2021-01-17 21:36:26 +0300

    [*] some trash...)

---
+++ MyPump/MVC/Test/CustomViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CustomViewController.swift
+//  MyPump
+//
+//  Created by Александр Майко on 08.01.2021.
+//
+
+import UIKit
+
+class CustomViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
