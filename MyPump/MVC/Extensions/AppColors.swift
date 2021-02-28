6fb245ecae5e23502d1df7d0ea701ef404fd0b94
Alex <alexmajko.1@gmail.com>
2021-02-28 18:56:11 +0300

    [*] add hex color extension

--- MyPump/MVC/Extensions/AppColors.swift
+++ MyPump/MVC/Extensions/AppColors.swift
@@ -10,8 +10,36 @@ import UIKit
 
 
 class AppColors {
-   static let detailsColor = UIColor(red: 255/255.0, green: 197/255.0, blue: 0/255.0, alpha: 1)
-    static let mainThemeColor = UIColor(red: 47/255.0, green: 79/255.0, blue: 79/255.0, alpha: 1)
-    static let secondThemeColor = UIColor(red: 154/255.0, green: 161/255.0, blue: 158/255.0, alpha: 1)
+    static let detailsColor = UIColor(hex:"#ffc500")
+    static let mainThemeColor = UIColor(hex: "#2f4f4f")
+    static let secondThemeColor = UIColor(hex: "#9aa19e")
 }
 
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
+    }
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format:"#%06x", rgb)
+    }
+}
