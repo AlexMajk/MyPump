7ca9a16513c2804235305e5f578cc66a8e3162e9
Alex <alexmajko.1@gmail.com>
2021-03-21 16:58:56 +0300

    [*] step to create shoppingbag

--- MyPump/MVC/Controllers/MainViewController/OrderViewController/OrderViewController.swift
+++ MyPump/MVC/Controllers/MainViewController/OrderViewController/OrderViewController.swift
@@ -8,11 +8,15 @@
 import UIKit
 
 class OrderViewController: UIViewController {
+
     
     var selectedObject: ObjectFromPartsCatalogueList?
     
     @IBOutlet weak var OrderTableView: UITableView!
     
+    @IBAction func addToShoppingBagButton(_ sender: UIBarButtonItem) {
+        ShopManager.addWatchList(selectedObject!)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,65 +75,65 @@ extension OrderViewController: UITableViewDelegate, UITableViewDataSource {
 
 
 
-class SpareViewController: UIViewController {
-
-
-    @IBOutlet weak var tableView: UITableView!
-
-    let shopManager = ShopManager.shared
-
-    let spareList: [ObjectFromPartsCatalogueList] = []
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.register(UINib(nibName: String(describing: ProductTableViewCell.self), bundle: nil), forCellReuseIdentifier: String(describing: ProductTableViewCell.self))
-        tableView.delegate = self
-        tableView.dataSource = self
-    }
-    
-    private func addProductToWatchList(selectedProduct: ObjectFromPartsCatalogueList) {
-        showAlertWithConfirmationInformation(product: selectedProduct)
-    }
-
-    private func showAlertWithConfirmationInformation(product: ObjectFromPartsCatalogueList) {
-        let alert = UIAlertController(title: "Вы хотите добавить выбранный товар в корзину?", message: nil, preferredStyle: .alert)
-        let okButton = UIAlertAction(title: "Да", style: .default) { (_) in
-            self.shopManager.addWatchList(product: product)
-        }
-        let noButton = UIAlertAction(title: "Нет", style: .cancel)
-
-        alert.addAction(okButton)
-        alert.addAction(noButton)
-        present(alert, animated: true, completion: nil)
-    }
-
-
-//    convenience init(spare: SpareModel) {
-//        self.init()
+//class SpareViewController: UIViewController {
+//
+//
+//    @IBOutlet weak var tableView: UITableView!
+//
+//    let shopManager = ShopManager.shared
+//
+//    let spareList: [ObjectFromPartsCatalogueList] = []
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        tableView.register(UINib(nibName: String(describing: ProductTableViewCell.self), bundle: nil), forCellReuseIdentifier: String(describing: ProductTableViewCell.self))
+//        tableView.delegate = self
+//        tableView.dataSource = self
 //    }
-}
-
-extension SpareViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return spareList.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ProductTableViewCell.self), for: indexPath) as! ProductTableViewCell
-        cell.configure(data: spareList[indexPath.row], isHiddenAddButton: false)
-        cell.delegate = self
-        return cell
-    }
-
-
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 40
-    }
-}
-
-extension SpareViewController: ProductTableViewCellDelegate {
-    func addPressButton(selectedProduct: ProductModel) {
-        addProductToWatchList(selectedProduct: selectedProduct)
-    }
-}
+//
+//    private func addProductToWatchList(selectedProduct: ObjectFromPartsCatalogueList) {
+//        showAlertWithConfirmationInformation(product: selectedProduct)
+//    }
+//
+//    private func showAlertWithConfirmationInformation(product: ObjectFromPartsCatalogueList) {
+//        let alert = UIAlertController(title: "Вы хотите добавить выбранный товар в корзину?", message: nil, preferredStyle: .alert)
+//        let okButton = UIAlertAction(title: "Да", style: .default) { (_) in
+//            self.shopManager.addWatchList(product: product)
+//        }
+//        let noButton = UIAlertAction(title: "Нет", style: .cancel)
+//
+//        alert.addAction(okButton)
+//        alert.addAction(noButton)
+//        present(alert, animated: true, completion: nil)
+//    }
+//
+//
+////    convenience init(spare: SpareModel) {
+////        self.init()
+////    }
+//}
+//
+//extension SpareViewController: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return spareList.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ProductTableViewCell.self), for: indexPath) as! ProductTableViewCell
+//        cell.configure(data: spareList[indexPath.row], isHiddenAddButton: false)
+//        cell.delegate = self
+//        return cell
+//    }
+//
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 40
+//    }
+//}
+//
+//extension SpareViewController: ProductTableViewCellDelegate {
+//    func addPressButton(selectedProduct: ProductModel) {
+//        addProductToWatchList(selectedProduct: selectedProduct)
+//    }
+//}
